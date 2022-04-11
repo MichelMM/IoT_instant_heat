302658be9fa1d44f15830c8d60121868ea572417
luisdelamoraf <luise_delamora@hotmail.com>
2022-04-10 20:00:31 -0500

    walkthroughth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instant_heat/splash2.dart';
-
+import 'package:dots_indicator/dots_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +17,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.amber,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      builder: (context,state){
-        return Splash2();
+      builder: (context, state) {
+        return const MyHomePage(title: 'Hola',);
       },
     );
   }
@@ -34,13 +34,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _currentPosition = 0.0;
+  final _totalDots = 3;
+
+
+  double _validPosition(double position) {
+    if (position >= _totalDots) return 0;
+    if (position < 0) return _totalDots - 1.0;
+    return position;
+  }
+
+  void _updatePosition(double position) {
+    setState(() => _currentPosition = _validPosition(position));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        body: Stack(
+      children: <Widget>[
+        PageView(
+          children: const <Widget>[
+            Walkthrougth(textContent: "Walkthrough one"),
+            Walkthrougth(textContent: "Walkthrough two"),
+            Walkthrougth(textContent: "Walkthrough tree"),
+          ],
+          onPageChanged: (double position) {setState(() => _currentPosition = _validPosition(position));} ,
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.7,
+          // left: MediaQuery.of(context).size.width * 0.35,
+          child: Padding(
+            padding:
+                EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.38),
+            child: Align(
+                alignment: Alignment.centerRight,
+                child: DotsIndicator(
+                  dotsCount: _totalDots,
+                  position: _currentPosition,
+                  decorator: const DotsDecorator(
+                      color: Colors.grey,
+                      activeColor: Color.fromARGB(255, 80, 80, 80)),
+                )),
+          ),
+        )
+      ],
+    ));
+  }
+}
 
-        title: Text(widget.title),
-      ),
+class Walkthrougth extends StatelessWidget {
+  final String textContent;
+  const Walkthrougth({Key? key, required this.textContent}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.redAccent),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Center(child: Text(textContent)),
     );
   }
 }
